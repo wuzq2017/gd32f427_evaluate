46738c384123a2fac25f277da9f4770a8633f0ce
wuzq <wdd_0227@163.com>
2022-11-30 22:41:58 +0800

    add gd demo and library

---
+++ GD32F4xx_Demo_Suites_V2.6.1/GD32427V_START_Demo_Suites/Projects/05_USB_MSC_Device/MDK-ARM/USBHS_Object/usbd_core.d
@@ -0,0 +1,51 @@
+./usbhs_object/usbd_core.o: \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\device\core\Source\usbd_core.c \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\device\core\Include\usbd_core.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\driver\Include\drv_usb_core.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\driver\Include\drv_usb_regs.h \
+  ..\inc\usb_conf.h C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\CMSIS\GD\GD32F4xx\Include\gd32f4xx.h \
+  C:\Users\wu\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\wu\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\wu\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\wu\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\wu\AppData\Local\Arm\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\mpu_armv7.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\CMSIS\GD\GD32F4xx\Include\system_gd32f4xx.h \
+  ..\inc\gd32f4xx_libopt.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_rcu.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_adc.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_can.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_crc.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_ctc.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_dac.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_dbg.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_dci.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_dma.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_exti.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_fmc.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_fwdgt.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_gpio.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_syscfg.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_i2c.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_iref.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_pmu.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_rtc.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_sdio.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_spi.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_timer.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_trng.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_usart.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_wwdgt.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_misc.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_enet.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_standard_peripheral\Include\gd32f4xx_exmc.h \
+  ..\..\..\Utilities\gd32f427v_start.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\ustd\common\usb_ch9_std.h \
+  ..\inc\usbd_conf.h ..\inc\usb_conf.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\driver\Include\drv_usb_dev.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\driver\Include\drv_usb_core.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\device\core\Include\usbd_enum.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\device\core\Include\usbd_core.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\wchar.h \
+  ..\..\..\..\GD32F4xx_Firmware_Library\GD32F4xx_usb_library\driver\Include\drv_usb_hw.h
